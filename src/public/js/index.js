0148c10592032873f0f5ae4ea10bb4feaf294a4a
Tomáš Diblík <dibla.tomas@post.cz>
2022-02-14 22:14:41 +0100

    [Wip] Add onHover animation on explore-it

---
+++ src/public/js/index.js
@@ -0,0 +1,5 @@
+function explore_it_onclick() {
+  // TODO: code lol
+  document.getElementById("rocket-transition-element").classList = "active";
+  setTimeout(function () {}, 2000); // When changing rocket animation time, please change it here as well
+}
