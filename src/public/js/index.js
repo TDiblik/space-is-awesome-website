247f05cc10a9f65fdd1e08bee685d2e40a6f4f1c
Tomáš Diblík <dibla.tomas@post.cz>
2022-02-15 00:33:50 +0100

    [Feature] Add page transition out of index

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,8 @@
 function explore_it_onclick() {
   // TODO: code lol
   document.getElementById("rocket-transition-element").classList = "active";
-  setTimeout(function () {}, 2000); // When changing rocket animation time, please change it here as well
+  document.getElementById("rocket-transition-ground").classList = "active";
+  setTimeout(function () {
+    window.location.href = "./subpages/Nasa.html";
+  }, 3600); // When changing rocket animation time, please change it here as well
 }
