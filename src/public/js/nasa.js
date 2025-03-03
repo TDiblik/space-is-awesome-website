fb1bb0b2aa487baa678fd9e9b65a46bbb8c1d5a2
Tomáš Diblík <dibla.tomas@post.cz>
2025-03-03 02:26:39 +0100

    [Fix] Fallback for .hdurl inside the nasa api call

--- src/public/js/nasa.js
+++ src/public/js/nasa.js
@@ -2,10 +2,8 @@ window.onload = function () {
   fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("picture-of-the-day").src = data.hdurl;
-      document.getElementById(
-        "picture-of-the-day-description"
-      ).innerText = `${data.title} -- "${data.explanation}"`;
+      document.getElementById("picture-of-the-day").src = data.hdurl ?? data.url;
+      document.getElementById("picture-of-the-day-description").innerText = `${data.title} -- "${data.explanation}"`;
     })
     .catch((error) => console.error(error));
 };
